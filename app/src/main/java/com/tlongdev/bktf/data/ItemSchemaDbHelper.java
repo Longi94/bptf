e7da7bddd9516da9caae60b85ab0987dda11b800
Long Tran <lngtrn94@gmail.com>
2015-03-13 20:55:16 +0100

    item db update

--- app/src/main/java/com/tlongdev/bktf/data/ItemSchemaDbHelper.java
+++ app/src/main/java/com/tlongdev/bktf/data/ItemSchemaDbHelper.java
@@ -20,8 +20,7 @@ import java.io.OutputStream;
 public class ItemSchemaDbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "items.db";
-    public static final int DATABASE_VERSION = 2;
-
+    public static final int DATABASE_VERSION = 3;
 
     public static final String TABLE_NAME = "itemschema";
     public static final String COLUMN_NAME = "name";
