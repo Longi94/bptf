8efbe5c07ec06101b05630af42e5a6668c88d01f
Long Tran <lngtrn94@gmail.com>
2015-03-23 17:25:17 +0100

    commented and reformatted view classes

--- app/src/main/java/com/tlongdev/bktf/view/SquareSmallFrameLayout.java
+++ app/src/main/java/com/tlongdev/bktf/view/SquareSmallFrameLayout.java
@@ -4,23 +4,48 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
+/**
+ * A square shaped frame layout that will always be third of the given size.
+ */
 public class SquareSmallFrameLayout extends FrameLayout {
 
+    /**
+     * Required constructor
+     *
+     * @param context context
+     */
     public SquareSmallFrameLayout(Context context) {
         super(context);
     }
 
+    /**
+     * Required constructor
+     *
+     * @param context context
+     * @param attrs   attributes
+     */
     public SquareSmallFrameLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    /**
+     * Required constructor
+     *
+     * @param context      context
+     * @param attrs        attributes
+     * @param defStyleAttr default style attributes
+     */
     public SquareSmallFrameLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //Square shape, third of the given size
         setMeasuredDimension(getMeasuredWidth() / 3, getMeasuredWidth() / 3);
     }
 }
