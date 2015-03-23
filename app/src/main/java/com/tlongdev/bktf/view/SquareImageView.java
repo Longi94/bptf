036668f1fdcc4f3b0df1a2cf6a792b1dcf8542d7
Long Tran <lngtrn94@gmail.com>
2015-03-23 17:25:17 +0100

    commented and reformatted view classes

--- app/src/main/java/com/tlongdev/bktf/view/SquareImageView.java
+++ app/src/main/java/com/tlongdev/bktf/view/SquareImageView.java
@@ -4,24 +4,48 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
+/**
+ * Square shaped image view.
+ */
 public class SquareImageView extends ImageView {
 
+    /**
+     * Required constructor
+     *
+     * @param context context
+     */
     public SquareImageView(Context context) {
         super(context);
     }
 
+    /**
+     * Required constructor
+     *
+     * @param context context
+     * @param attrs   attributes
+     */
     public SquareImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    /**
+     * Required constructor
+     *
+     * @param context      context
+     * @param attrs        attributes
+     * @param defStyleAttr default style attributes
+     */
     public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        //7:8 rectangle
+        //Square shape, the side of the sides are the size of the width
         setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
     }
 }
