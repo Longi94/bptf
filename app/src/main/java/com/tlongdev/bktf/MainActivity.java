da2cde88e9f488b0f88a4b5731b2114b49e1d8f4
Long Tran <lngtrn94@gmail.com>
2015-06-01 20:20:49 +0200

    fix more actionbaractivities

--- app/src/main/java/com/tlongdev/bktf/MainActivity.java
+++ app/src/main/java/com/tlongdev/bktf/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
--- app/src/main/java/com/tlongdev/bktf/UnusualActivity.java
+++ app/src/main/java/com/tlongdev/bktf/UnusualActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
 
