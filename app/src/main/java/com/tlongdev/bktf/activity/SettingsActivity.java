d48d7da8ef3172394c6f6aef0f11dbeee414d1ed
Long Tran <lngtrn94@gmail.com>
2015-11-17 00:44:54 +0100

    change dev e-mail address

--- app/src/main/java/com/tlongdev/bktf/activity/SettingsActivity.java
+++ app/src/main/java/com/tlongdev/bktf/activity/SettingsActivity.java
@@ -198,7 +198,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity implements
                     public boolean onPreferenceClick(Preference preference) {
                         //Start an email intent with my email as the target
                         Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                                "mailto", "tlongdev@gmail.com", null));
+                                "mailto", "dev@tlongdev.com", null));
                         if (intent.resolveActivity(getPackageManager()) != null) {
                             startActivity(Intent.createChooser(intent, getString(R.string.message_send_email)));
                         }
