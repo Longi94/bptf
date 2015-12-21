1b9c1991bb1b76be5e850b1b5651249c426d18e6
Long Tran <lngtrn94@gmail.com>
2015-12-21 19:23:13 +0100

    initial price widget

---
+++ app/src/main/java/com/tlongdev/bktf/widget/FavoritesWidget.java
@@ -0,0 +1,57 @@
+package com.tlongdev.bktf.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import com.tlongdev.bktf.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class FavoritesWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            // Construct the RemoteViews object
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.favorites_widget);
+
+            // Set up the intent that starts the StackViewService, which will
+            // provide the views for this collection.
+            Intent intent = new Intent(context, FavoritesWidgetService.class);
+
+            // Add the app widget ID to the intent extras.
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            // Set up the RemoteViews object to use a RemoteViews adapter.
+            // This adapter connects
+            // to a RemoteViewsService  through the specified intent.
+            // This is how you populate the data.
+            views.setRemoteAdapter(R.id.list_view, intent);
+
+            // The empty view is displayed when the collection has no items.
+            // It should be in the same layout used to instantiate the RemoteViews
+            // object above.
+            views.setEmptyView(R.id.list_view, R.id.empty_view);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
