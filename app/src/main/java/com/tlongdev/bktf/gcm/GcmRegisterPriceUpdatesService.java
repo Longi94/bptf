7e5df3fd8a7e8dca8adba0086b31db1d02d4e120
Long Tran <lngtrn94@gmail.com>
2015-12-24 15:29:50 +0100

    gifted and killstreak icons for future use

--- app/src/main/java/com/tlongdev/bktf/gcm/GcmRegisterPriceUpdatesService.java
+++ app/src/main/java/com/tlongdev/bktf/gcm/GcmRegisterPriceUpdatesService.java
@@ -18,7 +18,6 @@ import java.io.IOException;
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
  * <p/>
- * TODO: Customize class - update intent actions and extra parameters.
  */
 public class GcmRegisterPriceUpdatesService extends IntentService {
 
