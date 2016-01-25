4ea579f36bf593ee6dba52ab695c2f3b1b5b599d
Long Tran <lngtrn94@gmail.com>
2016-01-25 17:54:18 +0100

    added copyright notices

--- app/src/main/java/com/tlongdev/bktf/util/ParseLicenseXml.java
+++ app/src/main/java/com/tlongdev/bktf/util/ParseLicenseXml.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.tlongdev.bktf.util;
 
 import android.content.res.XmlResourceParser;
