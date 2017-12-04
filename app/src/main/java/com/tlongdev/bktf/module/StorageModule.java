45cf20e356d95ab2c93361b0f3d6f08b1237552d
Long Tran <lngtrn94@gmail.com>
2017-12-04 01:03:32 +0100

    move migration script to database class

--- app/src/main/java/com/tlongdev/bktf/module/StorageModule.java
+++ app/src/main/java/com/tlongdev/bktf/module/StorageModule.java
@@ -17,12 +17,9 @@
 package com.tlongdev.bktf.module;
 
 import android.app.Application;
-import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Room;
-import android.arch.persistence.room.migration.Migration;
 import android.content.ContentResolver;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.annotation.NonNull;
 
 import com.tlongdev.bktf.data.BptfDatabase;
 import com.tlongdev.bktf.data.DatabaseHelper;
@@ -43,37 +40,6 @@ import dagger.Provides;
  */
 @Module
 public class StorageModule {
-
-    private static final Migration MIGRATION_9_10 = new Migration(9, 10) {
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-            // migrating to room
-
-            // migrate unusual schema
-            database.execSQL("CREATE TABLE unusual_schema_copy (_id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL)");
-            database.execSQL("INSERT INTO unusual_schema_copy (_id, name) SELECT id, name FROM unusual_schema");
-            database.execSQL("DROP TABLE unusual_schema");
-            database.execSQL("ALTER TABLE unusual_schema_copy RENAME TO unusual_schema");
-
-            // migrate origins
-            database.execSQL("CREATE TABLE origin_names_copy (_id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL)");
-            database.execSQL("INSERT INTO origin_names_copy (_id, name) SELECT id, name FROM origin_names");
-            database.execSQL("DROP TABLE origin_names");
-            database.execSQL("ALTER TABLE origin_names_copy RENAME TO origin_names");
-
-            // migrate decorated weapons
-            database.execSQL("CREATE TABLE decorated_weapons_copy (_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, defindex INTEGER NOT NULL, grade INTEGER NOT NULL)");
-            database.execSQL("INSERT INTO decorated_weapons_copy (_id, defindex, grade) SELECT _id, defindex, grade FROM decorated_weapons");
-            database.execSQL("DROP TABLE decorated_weapons");
-            database.execSQL("ALTER TABLE decorated_weapons_copy RENAME TO decorated_weapons");
-
-            // migrate item schema
-            database.execSQL("CREATE TABLE item_schema_copy (_id INTEGER NOT NULL, defindex INTEGER NOT NULL, item_name TEXT NOT NULL, description TEXT, type_name TEXT NOT NULL, proper_name INTEGER NOT NULL, PRIMARY KEY(_id))");
-            database.execSQL("INSERT INTO item_schema_copy (_id, defindex, item_name, description, type_name, proper_name) SELECT _id, defindex, item_name, description, type_name, proper_name FROM item_schema");
-            database.execSQL("DROP TABLE item_schema");
-            database.execSQL("ALTER TABLE item_schema_copy RENAME TO item_schema");
-        }
-    };
 
     @Provides
     @Singleton
@@ -100,7 +66,7 @@ public class StorageModule {
     BptfDatabase provideBptfDatabase(Application application) {
         return Room.databaseBuilder(application, BptfDatabase.class, "bptf.db")
                 .addMigrations(
-                        MIGRATION_9_10
+                        BptfDatabase.MIGRATION_9_10
                 )
                 .allowMainThreadQueries()
                 .build();
